2882a02b793a3bf47252f2d4afbb78d0c1b81fe4
yousefhajjar <73161769+yousefhajjar@users.noreply.github.com>
2020-11-03 11:32:57 +0100

    delta time and border
    the movement was decreased and added a border for the player

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 0.7f;
+
+    float xMin, xMax, yMin, yMax;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
     }
 
     // Update is called once per frame
@@ -17,13 +22,28 @@ public class Player : MonoBehaviour
         Move();
     }
 
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
     private void Move()
     {
-        var deltaX = Input.GetAxis("Horizontal");
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var newXPos = transform.position.x + deltaX;
 
-        var deltaY = Input.GetAxis("Vertical");
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         var newYPos = transform.position.y + deltaY;
+
+        newYPos = Mathf.Clamp(newYPos, yMin, yMax);
 
         this.transform.position = new Vector2(newXPos, newYPos);
 
