fb054ebd01fa780bde98a57956e53d994d10534d
yousefhajjar <73161769+yousefhajjar@users.noreply.github.com>
2020-11-10 11:00:53 +0100

    laser/destroyer collision and waypoints
    hitbox of laser and destroy when it touches the shredder(a gameObject).
    paths and waypoints added.

---
+++ LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    //a list of points of type Transform
+    [SerializeField] List<Transform> waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ LaserDefender/Assets/Scripts/Shredder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        Destroy(otherObject.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print(collision.gameObject.name);
+        Destroy(collision.gameObject);
+    }
+}
