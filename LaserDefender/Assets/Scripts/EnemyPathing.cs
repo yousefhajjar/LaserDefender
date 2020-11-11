12fd217cb5e150e504ea22255008be19e9d30525
yousefhajjar <73161769+yousefhajjar@users.noreply.github.com>
2020-11-11 14:14:17 +0100

    enemymovement
    enemy moving towards waypoints and getting destroyed at the last one

--- LaserDefender/Assets/Scripts/EnemyPathing.cs
+++ LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -7,15 +7,47 @@ public class EnemyPathing : MonoBehaviour
     //a list of points of type Transform
     [SerializeField] List<Transform> waypoints;
 
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    //shows the next waypoint
+    int waypointIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //set the starting position of the Enemy ship to the position of the 1st waypoint
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyMove();
     }
+
+    private void EnemyMove()
+    {
+        if(waypointIndex < waypoints.Count)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+
+            targetPosition.z = 0f;
+
+            var enemyMovement = enemyMoveSpeed * Time.deltaTime;
+
+            //move from the current position, to the target position, at the enemyMovement speed
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, enemyMovement);
+            
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
